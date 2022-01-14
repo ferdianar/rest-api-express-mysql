fa8bfe53622e986c979bc3b5746535bb907ce21e
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-14 17:48:22 +0700

    initial and config library'

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var express = require('express')
+
+// initialize PATH
 var path = require('path')
+
+// initialize Cookie Parser
 var cookieParser = require('cookie-parser')
+
+// import Morgan Logger
 var logger = require('morgan')
 
 // Routing
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 
+// create express server
 var app = express()
 
+// initialize Middlewares
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
